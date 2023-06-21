831c92ff968e838d832637bb4ee359a8c3f3ad31
Фукиш <egor158741@yandex.ru>
2023-06-21 18:04:47 +0300

    Using buffer for deframer in RAM

--- src/deframer.py
+++ src/deframer.py
@@ -1,4 +1,4 @@
-import bitstring, Levenshtein, argparse, time
+import Levenshtein, argparse, time, io
 parser=argparse.ArgumentParser()
 parser.add_argument("-o", "--output", help="Output binary file name")
 parser.add_argument("-i", "--input", help="Input binary file name")
@@ -11,9 +11,8 @@ def pn_derandomizer(data, mask):
 def byte_inverter(derand):
     chunks=[derand[i:i+8] for i in range(0, len(derand), 8)]
     f=chunks[::2]
-    two_invert=bitstring.BitArray(bin=''.join(chunks[1:][::2]))
-    two_invert.invert()
-    s=two_invert.bin
+    s=''.join(chunks[1:][::2])
+    s=''.join(['1' if i == '0' else '0' for i in s])
     s=[s[i:i+8] for i in range(0, len(s), 8)]
     return [''.join(x) for x in zip(f, s)]
 
@@ -29,7 +28,7 @@ def skip_end_frame(f):
     while(byte := f.read(5025)):
         return
     
-def main(input_file, sync_marker, sync_buffer, mask, out):
+def main(input_file, sync_marker, sync_buffer, mask, out, out_filename):
     bit_array=''
     k=0
     n=0
@@ -46,24 +45,25 @@ def main(input_file, sync_marker, sync_buffer, mask, out):
                 n+=1
                 print('New frame! '+str(n)+' | Sync word: 0x'+str(hex(int(sync_buffer, 2)))[-16:].upper()+' | BER: '+str(round(float(err/10.24), 1))+'% | Sync threshold: '+str(round(float(err/3.68), 1))+'%'+'     ', end='\r')
                 frame=get_frame_bytes(f=f)
-                #frame=int(str(bit_array)+str(frame[:int(354848-len(bit_array))]), 2)
                 frame=int(str(bit_array[1:])+str(frame[:int(354848-len(bit_array[1:]))]), 2)
                 derand = pn_derandomizer(data=frame, mask=mask)
                 out.write(int(''.join(byte_inverter(derand)), 2).to_bytes(44356, byteorder='big', signed=False))
                 skip_end_frame(f=f)
-        else:
+        if(bits_array==None):
+            with open(out_filename, 'wb') as out_bytes_file:
+                out_bytes_file.write(out.getvalue())
             input_file.close()
             break
     return
 
 if(__name__=='__main__'):
     f=open(inputfile, "rb")
-    out_file=open(outfile, 'ab')
+    out_file=io.BytesIO()
     start_time = time.time()
     sync = '72CB2EBAE79E5145E79C5149E7B451B9E5945D79CF14A27BCD18AE53E5E85C71C924B6DBB6D9B6D5B6FDB60DB42DB8ED926D6D6F6F63634B4BBBB99995557FFF'
-    sync_marker=bitstring.BitArray(hex=sync).bin
+    sync_marker=bin(int(sync, 16))[2:].zfill(int(len(sync)/2)*8)
     sync_buffer=str('0'*int(len(sync_marker)))
     with open('fy2_mask.bin', 'rb') as file:
-        mask=int(bitstring.ConstBitStream(file).read(354848).uint)
-    main(input_file=f, sync_buffer=sync_buffer, sync_marker=sync_marker, mask=mask, out=out_file)
+        mask=int.from_bytes(file.read(44356), byteorder='big', signed=False)
+    main(input_file=f, sync_buffer=sync_buffer, sync_marker=sync_marker, mask=mask, out=out_file, out_filename=outfile)
     print("--- %s seconds ---" % (time.time() - start_time)+str(' '*50))
