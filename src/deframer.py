566aa25b706d87071ca18ff790eb72dddf736e53
Фукиш <egor158741@yandex.ru>
2023-09-14 23:53:00 +0300

    Add PN mask generator in deframer

--- src/deframer.py
+++ src/deframer.py
@@ -1,9 +1,19 @@
-import Levenshtein, argparse, time, io
+import Levenshtein, argparse, time, io, os
 parser=argparse.ArgumentParser()
 parser.add_argument("-o", "--output", help="Output binary file name")
 parser.add_argument("-i", "--input", help="Input binary file name")
 inputfile=parser.parse_args().input
 outfile=parser.parse_args().output
+
+def PN_mask_generator(mask):
+    out=[]
+    for _ in range(354848):
+        x2=mask[14:]
+        x1=mask[13:14]
+        xor1=int(x1)^int(x2)
+        mask=str(xor1)+mask[:14]
+        out.append(mask[14:])
+    return int(''.join(out), 2)
 
 def pn_derandomizer(data, mask):
     return str(bin(int(mask)^int(data)))[2:].zfill(354848)
@@ -28,11 +38,11 @@ def skip_end_frame(f):
     while(byte := f.read(5025)):
         return
     
-def main(input_file, sync_marker, sync_buffer, mask, out, out_filename):
+def main(input_file, sync_marker, sync_buffer, mask, out, out_filename, total_len):
     bit_array=''
     k=0
     n=0
-    while True:
+    for _ in range(total_len):
         if(len(bit_array)<=1):
             bits_array=get_byte(f=input_file)
             k=0
@@ -43,7 +53,7 @@ def main(input_file, sync_marker, sync_buffer, mask, out, out_filename):
             err=int(Levenshtein.hamming(sync_buffer, str(sync_marker)))
             if(err<=int(158)): #158
                 n+=1
-                print('New frame! '+str(n)+' | Sync word: 0x'+str(hex(int(sync_buffer, 2)))[-16:].upper()+' | BER: '+str(round(float(err/10.24), 1))+'% | Sync threshold: '+str(round(float(err/3.68), 1))+'%'+'     ', end='\r')
+                print(f'New frame! {n} | Sync word: 0x{str(hex(int(sync_buffer, 2)))[-16:].upper()} | BER: {round(float(err/10.24), 1)}% | Sync threshold: {round(float(err/3.68), 1)}%     ', end='\r')
                 frame=get_frame_bytes(f=f)
                 frame=int(str(bit_array[1:])+str(frame[:int(354848-len(bit_array[1:]))]), 2)
                 derand = pn_derandomizer(data=frame, mask=mask)
@@ -58,12 +68,13 @@ def main(input_file, sync_marker, sync_buffer, mask, out, out_filename):
 
 if(__name__=='__main__'):
     f=open(inputfile, "rb")
+    total_len=int(os.path.getsize(inputfile)*8)
+    print(f'Total bits: {total_len}')
     out_file=io.BytesIO()
     start_time = time.time()
     sync = '72CB2EBAE79E5145E79C5149E7B451B9E5945D79CF14A27BCD18AE53E5E85C71C924B6DBB6D9B6D5B6FDB60DB42DB8ED926D6D6F6F63634B4BBBB99995557FFF'
     sync_marker=bin(int(sync, 16))[2:].zfill(int(len(sync)/2)*8)
     sync_buffer=str('0'*int(len(sync_marker)))
-    with open('fy2_mask.bin', 'rb') as file:
-        mask=int.from_bytes(file.read(44356), byteorder='big', signed=False)
-    main(input_file=f, sync_buffer=sync_buffer, sync_marker=sync_marker, mask=mask, out=out_file, out_filename=outfile)
+    mask=PN_mask_generator(mask='000000000000001')
+    main(input_file=f, sync_buffer=sync_buffer, sync_marker=sync_marker, mask=mask, out=out_file, out_filename=outfile, total_len=total_len)
     print("--- %s seconds ---" % (time.time() - start_time)+str(' '*50))
