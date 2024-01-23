55f7e461e9a54123eb33d0453f6dd35303949066
Фукиш <egor158741@yandex.ru>
2024-01-24 00:16:08 +0300

    Added a mechanism for skipping noisy chunks!

--- src/deframer.py
+++ src/deframer.py
@@ -37,10 +37,21 @@ def skip_end_frame(f):
     while(byte := f.read(5025)):
         return
 
-def main(input_file, sync_marker, sync_buffer, mask, out, out_filename, total_len):
+def skip_frame(f):
+    while(byte := f.read(int(49381*2))):
+        return
+
+def multi_skip(f):
+    while(byte := f.read(int(49381*5))):
+        return
+
+def main(input_file, sync_buffer, mask, out, out_filename, total_len):
     bit_array=''
     k=0
     n=0
+    bit_noise_counter=0
+    multi_skip_counter=0
+    sync_state='NO SYNC!'
     for _ in range(int(total_len+100)):
         if(len(bit_array)<=1):
             bits_array=get_byte(f=input_file)
@@ -51,13 +62,30 @@ def main(input_file, sync_marker, sync_buffer, mask, out, out_filename, total_le
             sync_buffer=sync_buffer[1:]+bit_array[:1]
             err=int(hamming_distance_string(hex(int(sync_buffer,2))[2:].zfill(128), sync))
             if(err<=int(158)): #158
+                bit_noise_counter=0
+                multi_skip_counter=0
+                sync_state='SYNCED!'
                 n+=1
-                print(f'New frame! {n} | Sync word: 0x{str(hex(int(sync_buffer, 2)))[-16:].upper()} | BER: {round(float(err/5.12), 1)}% | Sync threshold: {round(float(err/1.58), 1)}%     ', end='\r')
-                frame=get_frame_bytes(f=f)
+                print(f'Frame: {n} | State: {sync_state} | Sync word: 0x{str(hex(int(sync_buffer, 2)))[-16:].upper()} | BER: {str(round(float(err/5.12), 1)).rjust(4, " ")}% | Sync threshold: {round(float(err/1.58), 1)}%     ', end='\r')
+                frame=get_frame_bytes(f=input_file)
                 frame=int(str(bit_array[1:])+str(frame[:int(354848-len(bit_array[1:]))]), 2)
                 derand = pn_derandomizer(data=frame, mask=mask)
                 out.write(int(''.join(byte_inverter(derand)), 2).to_bytes(44356, byteorder='big', signed=False))
-                skip_end_frame(f=f)
+                skip_end_frame(f=input_file)
+            else:
+                bit_noise_counter+=1
+            if(bit_noise_counter>=791120):
+                if(multi_skip_counter>=10):
+                    multi_skip(f=input_file)
+                    multi_skip_counter=0
+                    sync_state='NO SYNC!'
+                    print(f'Frame: {n} | State: {sync_state} | Sync word: 0x{str(hex(int(sync_buffer, 2)))[-16:].upper()} | BER: {str(round(float(err/5.12), 1)).rjust(4, " ")}% | Sync threshold: {round(float(err/1.58), 1)}%     ', end='\r')
+                else:
+                    sync_state='NO SYNC!'
+                    print(f'Frame: {n} | State: {sync_state} | Sync word: 0x{str(hex(int(sync_buffer, 2)))[-16:].upper()} | BER: {str(round(float(err/5.12), 1)).rjust(4, " ")}% | Sync threshold: {round(float(err/1.58), 1)}%     ', end='\r')
+                    skip_frame(f=input_file)
+                    multi_skip_counter+=1
+                bit_noise_counter=0
         if(bits_array==None):
             print('Saving data...')
             with open(out_filename, 'wb') as out_bytes_file:
@@ -76,5 +104,5 @@ if(__name__=='__main__'):
     sync_marker=bin(int(sync, 16))[2:].zfill(int(len(sync)/2)*8)
     sync_buffer=str('0'*int(len(sync_marker)))
     mask=PN_mask_generator(mask=0x01)
-    main(input_file=f, sync_buffer=sync_buffer, sync_marker=sync_marker, mask=mask, out=out_file, out_filename=outfile, total_len=total_len)
+    main(input_file=f, sync_buffer=sync_buffer, mask=mask, out=out_file, out_filename=outfile, total_len=total_len)
     print("--- %s seconds ---" % (time.time() - start_time)+str(' '*50))
